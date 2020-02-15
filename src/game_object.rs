45b2cf6d0d1ce8caf8f69d2357bb563e48cd5000
Kamil Pituła <pitula.k@gmail.com>
2020-02-15 11:36:55 +0100

    game object fix

--- src/game_object.rs
+++ src/game_object.rs
@@ -1,5 +1,6 @@
-use piston::input::UpdateArgs;
+use piston::input::{RenderArgs, UpdateArgs, Button};
 
 pub trait Game_Object {
     fn update(&mut self, args: &UpdateArgs);
+    fn key_press(&mut self, args: &Button);
 }
